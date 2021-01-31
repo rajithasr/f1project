ea3fc0831adf2c2b1eb69ee1af9ae7ee98ec66ac
Rajitha <rajitha.sadhula@gmail.com>
2021-01-31 15:17:08 +0530

    first commit

---
+++ src/test/java/tc/tc_homepage2.java
@@ -0,0 +1,14 @@
+package tc;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class tc_homepage2 {
+	@Test 
+	public void openhome()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com");
+	}
+
+}
